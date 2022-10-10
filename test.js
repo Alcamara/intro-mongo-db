323636da038c68c82cfbfd6053ea27d5dc5dbc5a
Al Camara <alcamara763@Als-MacBook-Pro.local>
2022-10-10 13:52:41 -0500

    create entry in data base

--- test.js
+++ test.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 connect = () =>{
    // returns a promise      /* web protocal:url*/
-	return mongoose.connect('mongoosedb://localhost:27017')
+	return mongoose.connect('mongodb://localhost:27017',{
+        useNewUrlParser: true
+      })
 }
 
 const student = new mongoose.Schema({
@@ -11,3 +13,19 @@ const student = new mongoose.Schema({
 });
 
 const Student = mongoose.model('student',student);
+
+connect()
+ .then(async connection =>{
+
+    console.log('test');
+
+    const newStudent = await Student.create({firstName: '007 Bond'});
+
+
+    
+    console.log(newStudent);
+
+ })
+ .catch(e =>{
+    console.error(` Error: ${e}`);
+ })
