b6291495fa71ce5f9fe03b0c34c9baaf01f61623
Al Camara <Alhousseinec@gmail.com>
2022-10-11 22:55:14 -0500

    learn querying documents

--- test.js
+++ test.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 connect = () =>{
    // returns a promise      /* web protocal:url*/
 	return mongoose.connect('mongodb://localhost:27017',{
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
       })
 }
 
 const student = new mongoose.Schema({
     //firstName: String,
     // if your want to add more metadate
-   firstname: {
+   firstName: {
       type: String,
       // validation
       required: true,
@@ -18,15 +19,15 @@ const student = new mongoose.Schema({
       unique: true
    },
 
-   faveFoods: [{type: string}],
-   info: {
-      school:{
-         type: string
-      },
-      showSize: {
-         type: Number
-      }
-   },
+   // faveFoods: [{type: String}],
+   // info: {
+   //    school:{
+   //       type: String
+   //    },
+   //    showSize: {
+   //       type: Number
+   //    }
+   // },
 
    school: {
       type: mongoose.Schema.Types.ObjectId,
@@ -34,10 +35,14 @@ const student = new mongoose.Schema({
       ref: 'school'
    }
 
-},{timestamps: true});
+});
 
 const school = new mongoose.Schema({
-   name: String
+   name: String,
+   openSince: Number,
+   students: Number,
+   isGreat: Boolean,
+   staff:[{type: String}] 
 })
 
 const School = mongoose.model('school',school);
@@ -51,16 +56,67 @@ connect()
  
    //  console.log(student);
 
-   const addSchool =  await School.create({name: 'MIT'});
+   const schoolConfig = {
+      name: 'Armstrong High School',
+      openSince: 1985,
+      students: 800,
+      isGreat: true
+   }
 
-   const newStudent = await Student.create({
-      firstname: 'Paul', 
-      school: addSchool._id,
+   const schoolConfig2 = {
+      name: 'Johnson High School',
+      openSince: 1975,
+      students: 400,
+      isGreat: false,
+      
+   }
+
+   const schoolConfig3 = {
+      name: 'Carter High School',
+      openSince: 1965,
+      students: 400,
+      isGreat: false,
+      staff:['Mike','Chris','Peter'] 
+   }
+
+   const schoolConfig4 = {
+      name: 'George Washingston High School',
+      openSince: 1945,
+      students: 1000,
+      isGreat: true,
+      staff:['Sara','Ben','Charle'] 
+   }
+
+   const newSchools = await School.create(schoolConfig4)
+
+   /*
+      $gt = great than
+   */
+
+   // const match = await School.findOne({
+   //    students: {$gt: 300},
+   //    isGreat: false
+   
+   // }).exec()
+
+   const match = await School.find({
+      staff: 'Peter',
+      isGreat: false
+   
    })
+   .limit(2) //limit amount of documents returned
+   .exec()
 
-   const match = await Student.findById(student.id)
-      .populate('school')
-      .exec()
+   // const addSchool =  await School.create({name: 'MIT'});
+
+   // const newStudent = await Student.create({
+   //    firstName: 'Joe', 
+   //    school: addSchool._id,
+   // })
+
+   // const match = await Student.findById(newStudent.id)
+   //    .populate('school')
+   //    .exec()
 
    console.log(match);
 
